980f5c0965866af2eac6e30aab16c4f4418cc423
Kris Doyon <kristopher.doyon@gmail.com>
2022-09-13 19:30:42 -0400

    Refactor functions

--- js/script.js
+++ js/script.js
@@ -1,21 +1,19 @@
 "use strict";
 
-let questionTimer;
+let questionTimer, gameTimer;
 
-const triviaGame = {
-  category: undefined,
-  categoryStr: undefined,
+const game = {
+  category: "",
+  categoryStr: "",
+  currentQuestion: 0,
+  currentScore: 0,
+  answered: false,
   questions: [],
   incorrectAnswers: [],
   correctAnswers: [],
   answers: [],
-  currentQuestion: 0,
-  score: 0,
-  answered: false,
   correctIndexes: [],
 };
-
-// https://the-trivia-api.com/api/questions?categories=arts_and_literature,film_and_tv,food_and_drink,general_knowledge,geography,history,music,science,society_and_culture,sport_and_leisure&limit=10
 
 const categoriesMap = new Map([
   ["Arts & Literature", "arts_and_literature"],
@@ -30,78 +28,49 @@ const categoriesMap = new Map([
   ["Sports & Leisure", "sport_and_leisure"],
 ]);
 
-// const categoriesMap = new Map([
-//   ["Animals", 27],
-//   ["Books", 10],
-//   ["General", 9],
-//   ["Geography", 22],
-//   ["History", 23],
-//   ["Math", 19],
-//   ["Movies", 11],
-//   ["Music", 12],
-//   ["Politics", 24],
-//   ["Science", 17],
-//   ["TV", 14],
-// ]);
-
 const categoriesStrs = Array.from(categoriesMap.values());
 
 ///////////////////////////////////////////////////////////////////
 // ELEMENTS
 
-// BOXES
-const boxIntroEl = document.querySelector(".box-intro");
-const boxCategoriesEl = document.querySelector(".box-choose-category");
-const boxTimerEl = document.querySelector(".box-timer");
-const boxGameEl = document.querySelector(".box-game");
-const boxTryAgainEl = document.querySelector(".box-try-again");
+const boxes = document.querySelectorAll(".box");
+
+// INDIVIDUAL BOXES
+const boxIntro = document.querySelector(".box-intro");
+const boxCategories = document.querySelector(".box-choose-category");
+const boxTimer = document.querySelector(".box-timer");
+const boxGame = document.querySelector(".box-game");
+const boxTryAgain = document.querySelector(".box-try-again");
 
 // INDIVIDUAL BUTTONS
-const buttonNewEl = document.querySelector(".btn-new-game");
-const animalsButtonEl = document.querySelector("#btn-animals");
-const booksButtonEl = document.querySelector("#btn-books");
-const generalButtonEl = document.querySelector("#btn-general");
-const geographyButtonEl = document.querySelector("#btn-geography");
-const historyButtonEl = document.querySelector("#btn-history");
-const mathButtonEl = document.querySelector("#btn-math");
-const moviesButtonEl = document.querySelector("#btn-movies");
-const musicButtonEl = document.querySelector("#btn-music");
-const politicsButtonEl = document.querySelector("#btn-politics");
-const scienceButtonEl = document.querySelector("#btn-science");
-const tvButtonEl = document.querySelector("#btn-tv");
-const randomButtonEl = document.querySelector("#btn-random");
-const backButtonEl = document.querySelector(".btn-back");
-const nextButtonEl = document.querySelector(".btn-next");
-const playAgainButtonEl = document.querySelector(".btn-play-again");
-const tryAgainButtonEl = document.querySelector(".btn-try-again");
+const buttonNew = document.querySelector(".btn-new-game");
+const buttonNext = document.querySelector(".btn-next");
+const buttonPlayAgain = document.querySelector(".btn-play-again");
+const buttonTryAgain = document.querySelector(".btn-try-again");
+const buttonStartOver = document.querySelector(".start-over");
 
-// CATEGORY BUTTONS NODELISTS
+// CATEGORY BUTTONS NODELIST
 const categoryButtons = document.querySelectorAll(".btn-category");
 
-// GAMEPLAY ELEMENTS
+// ANSWER BUTTONS NODELIST
+const answerButtons = document.querySelectorAll(".btn-answer-choice");
+
+// GAMEPLAY ELEMENT LABELS
 const labelCurrentQuestion = document.querySelector(".current-question");
 const labelScore = document.querySelector(".current-score");
 const labelCategory = document.querySelector(".current-category");
 const labelTimer = document.querySelector(".countdown-timer");
-const labelTimeRemaining = document.querySelector(".time-remaining");
+const labelQuestionTimer = document.querySelector(".label-question-timer");
 const labelQuestionNumber = document.querySelector(".title-question");
-const answerButtons = document.querySelectorAll(".btn-answer-choice");
 
+// questionTim
 const questionTimerEl = document.querySelector(".question-timer");
 
 ///////////////////////////////////////////////////////////////////
 // FUNCTIONS
 
-const getQuestions = function (game) {
+const getQuestions = function () {
   const xhr = new XMLHttpRequest();
-
-  // USING OPEN TRIVIA DB
-  // xhr.open(
-  //   "GET",
-  //   `https://opentdb.com/api.php?amount=10&category=${game.categoryNumber}&type=multiple`
-  // );
-
-  // USING THE TRIVIA API
   xhr.open(
     "GET",
     `https://the-trivia-api.com/api/questions?categories=${game.categoryStr}&limit=10`
@@ -120,21 +89,18 @@ const getQuestions = function (game) {
   };
 };
 
-const startGameTimer = function (game) {
+const startGameTimer = function () {
   const tick = function () {
     labelTimer.textContent = timeRemaining;
     if (timeRemaining === 0) {
       clearInterval(timer);
-      updateScore(game);
-      boxTimerEl.classList.add("hidden");
-      if (game.data.length) {
-        randomizeAnswers(game);
-        boxGameEl.classList.remove("hidden");
-        updateQuestion(triviaGame);
-        console.log("yes data");
+      updateScore();
+      if (game.correctAnswers.length) {
+        randomizeAnswers();
+        displayBox(boxGame);
+        updateQuestion();
       } else {
-        boxTryAgainEl.classList.remove("hidden");
-        console.log("no data");
+        displayBox(boxTryAgain);
       }
     }
     timeRemaining--;
@@ -145,20 +111,18 @@ const startGameTimer = function (game) {
   return timer;
 };
 
-const startQuestionTimer = function (game) {
+const startQuestionTimer = function () {
   const tick = function () {
-    labelTimeRemaining.textContent = timeRemaining;
-    console.log(timeRemaining);
+    labelQuestionTimer.textContent = timeRemaining;
     if (timeRemaining === 5) {
       questionTimerEl.classList.add("low-time");
     }
     if (timeRemaining === 0) {
       clearInterval(timer);
-      revealCorrectAnswer(game);
-      answerButtons.forEach((button) => button.classList.add("no-hover"));
-      displayButton(game);
+      revealCorrectAnswer();
+      displayButton();
       game.currentQuestion++;
-      updateScore(game);
+      updateScore();
     }
     timeRemaining--;
   };
@@ -168,32 +132,33 @@ const startQuestionTimer = function (game) {
   return timer;
 };
 
-const randomizeAnswers = function (game) {
-  game.incorrectAnswers.forEach((answersArr, i) => {
+const randomizeAnswers = function () {
+  game.incorrectAnswers.forEach((incorrectAnswersArr, i) => {
     game.answers.push(
-      [...answersArr, game.correctAnswers[i]].sort(() => Math.random() - 0.5)
+      [...incorrectAnswersArr, game.correctAnswers[i]].sort(
+        () => Math.random() - 0.5
+      )
     );
     game.correctIndexes.push(game.answers[i].indexOf(game.correctAnswers[i]));
   });
 };
 
-const revealCorrectAnswer = function (game) {
+const revealCorrectAnswer = function () {
   answerButtons[game.correctIndexes[game.currentQuestion]].classList.add(
     "correct-answer"
   );
+  answerButtons.forEach((button) => button.classList.add("no-hover"));
 };
 
-// const startQuestionTimer = function(game) {};
-
-const updateQuestion = function (game) {
-  questionTimer = startQuestionTimer(game);
+const updateQuestion = function () {
+  questionTimer = startQuestionTimer();
   questionTimerEl.classList.remove("low-time");
   game.answered = false;
   labelQuestionNumber.textContent = `Question #${game.currentQuestion + 1}:`;
   labelCurrentQuestion.innerHTML = game.questions[game.currentQuestion];
   answerButtons.forEach((button) => button.classList.remove("no-hover"));
-  nextButtonEl.classList.add("hidden");
-  questionTimerEl.classList.remove("hidden");
+  displayBox(boxGame);
+  displayQuestionTimer();
 
   answerButtons.forEach((button, i) => {
     button.innerHTML = game.answers[game.currentQuestion][i];
@@ -203,17 +168,17 @@ const updateQuestion = function (game) {
   });
 };
 
-const updateScore = function (game) {
-  labelScore.textContent = `${game.score} / ${game.currentQuestion}`;
+const updateScore = function () {
+  labelScore.textContent = `${game.currentScore} / ${game.currentQuestion}`;
 };
 
-const updateCategory = function (game) {
+const updateCategory = function () {
   labelCategory.textContent = `${game.category}`;
 };
 
-const resetGame = function (game) {
+const resetGame = function () {
   game.currentQuestion = 0;
-  game.score = 0;
+  game.currentScore = 0;
   game.answers = [];
   game.correctIndexes = [];
   game.category = undefined;
@@ -222,113 +187,93 @@ const resetGame = function (game) {
   game.incorrectAnswers = [];
   game.correctAnswers = [];
   game.answered = false;
-  boxGameEl.classList.add("hidden");
-  boxCategoriesEl.classList.remove("hidden");
-  updateScore(game);
-  answerButtons.forEach((button, i) => {
-    // button.innerHTML = game.answers[game.currentQuestion][i];
+  displayBox(boxCategories);
+  updateScore();
+  answerButtons.forEach((button) => {
     button.classList.remove("correct-answer");
     button.classList.remove("incorrect-answer");
     button.classList.remove("selected-answer");
   });
-  playAgainButtonEl.classList.add("hidden");
+  gameTimer && clearInterval(gameTimer);
+  questionTimer && clearInterval(questionTimer);
 };
 
-const randomCategory = function (game) {
+const randomCategory = function () {
   game.categoryStr =
     categoriesStrs[Math.trunc(Math.random() * categoriesStrs.length)];
 };
 
 const displayQuestionTimer = function () {
   questionTimerEl.classList.remove("hidden");
-  playAgainButtonEl.classList.add("hidden");
-  nextButtonEl.classList.add("hidden");
+  buttonPlayAgain.classList.add("hidden");
+  buttonNext.classList.add("hidden");
 };
 
-const displayButton = function (game) {
-  const button = game.currentQuestion < 9 ? nextButtonEl : playAgainButtonEl;
-  setTimeout(function () {
-    questionTimerEl.classList.add("hidden");
-    button.classList.remove("hidden");
-  }, 1500);
+const displayButton = function () {
+  const button = game.currentQuestion < 9 ? buttonNext : buttonPlayAgain;
+  questionTimerEl.classList.add("hidden");
+  button.classList.remove("hidden");
 };
 
-const initializeGame = function (game) {
-  // Show the timer box
-  boxTimerEl.classList.remove("hidden");
-  // Get questions from API
-  getQuestions(game);
-  // Start the countdown timer
-  startGameTimer(game);
+const initializeGame = function () {
+  displayBox(boxTimer);
+  getQuestions();
+  gameTimer = startGameTimer();
+  updateScore();
+};
 
-  // setTimeout(function () {
-  //   boxGameEl.classList.remove("hidden");
-  //   updateQuestion(triviaGame);
-  // }, 3000);
+const displayBox = function (boxToShow) {
+  boxes.forEach((box) => box.classList.add("hidden"));
+  boxToShow.classList.remove("hidden");
 };
 
 ///////////////////////////////////////////////////////////////////
 // EVENT HANDLERS
 
 // NEW GAME BUTTON
-buttonNewEl.addEventListener("click", function () {
-  boxIntroEl.classList.add("hidden");
-  boxCategoriesEl.classList.remove("hidden");
+buttonNew.addEventListener("click", function () {
+  displayBox(boxCategories);
 });
 
 // CATEGORY BUTTONS
-
 categoryButtons.forEach((button) => {
   button.addEventListener("click", function () {
-    // Set the category to the button text content
-    triviaGame.category = labelCategory.textContent =
+    game.category = labelCategory.textContent =
       button.getElementsByClassName("title-category")[0].textContent;
-    // Generate random category number
-    if (triviaGame.category === "Random") {
-      randomCategory(triviaGame);
-      // Get category number from categories map
-    } else {
-      triviaGame.categoryStr = categoriesMap.get(
-        button.getElementsByClassName("title-category")[0].textContent
-      );
-    }
-    // Hide the categories box
-    boxCategoriesEl.classList.add("hidden");
-
-    initializeGame(triviaGame);
+    game.categoryStr = categoriesMap.get(
+      button.getElementsByClassName("title-category")[0].textContent
+    );
+    initializeGame();
   });
 });
 
 answerButtons.forEach((button, i) => {
   button.addEventListener("click", function () {
-    if (!triviaGame.answered && triviaGame.currentQuestion <= 9) {
+    if (!game.answered && game.currentQuestion <= 9) {
       clearInterval(questionTimer);
-      if (triviaGame.correctIndexes[triviaGame.currentQuestion] === i) {
-        triviaGame.score++;
+      if (game.correctIndexes[game.currentQuestion] === i) {
+        game.currentScore++;
       } else {
         button.classList.add("incorrect-answer");
       }
-      revealCorrectAnswer(triviaGame);
-      answerButtons[
-        triviaGame.correctIndexes[triviaGame.currentQuestion]
-      ].classList.add("correct-answer");
+      revealCorrectAnswer();
 
-      answerButtons.forEach((button) => button.classList.add("no-hover"));
-
-      displayButton(triviaGame);
-      triviaGame.currentQuestion++;
-      updateScore(triviaGame);
+      displayButton();
+      game.currentQuestion++;
+      updateScore();
       button.classList.add("selected-answer");
-      triviaGame.answered = true;
+      game.answered = true;
     }
   });
 });
 
-nextButtonEl.addEventListener("click", updateQuestion.bind(null, triviaGame));
+buttonNext.addEventListener("click", updateQuestion);
 
-playAgainButtonEl.addEventListener("click", resetGame.bind(null, triviaGame));
+buttonPlayAgain.addEventListener("click", resetGame);
 
-tryAgainButtonEl.addEventListener(
-  "click",
-  initializeGame.bind(null, triviaGame)
-);
+buttonTryAgain.addEventListener("click", initializeGame);
+
+buttonStartOver.addEventListener("click", function () {
+  displayBox(boxCategories);
+  resetGame();
+});
